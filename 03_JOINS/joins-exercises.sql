eb47daca392262a1261b93e7b6877a0238cc1406
Student <student@Techs-MacBook-Air.local>
2021-02-10 19:36:37 -0500

    Completed pair exercises

--- 03_JOINS/joins-exercises.sql
+++ 03_JOINS/joins-exercises.sql
@@ -43,15 +43,37 @@ WHERE co.continent = 'Europe';
 
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
+SELECT ROUND(AVG(c.population))
+FROM city c
+JOIN country co ON co.code = c.countrycode
+WHERE co.continent = 'Asia';
 
 -- 7. The number of cities in countries where English is an official language
 -- (number of cities where English is official language: 523)
+SELECT Count (c.name)
+FROM city c JOIN country co ON co.code = c.countrycode
+JOIN countrylanguage cl ON cl.countrycode = co.code
+WHERE cl.language = 'English' AND cl.isOfficial = 'true';
 
 -- 8. The average population of cities in countries where the official language is English
 -- (average population of cities where English is official language: 285,809)
+SELECT Round(AVG(c.population))
+FROM city c JOIN country co ON co.code = c.countrycode
+JOIN countrylanguage cl ON cl.countrycode = co.code
+WHERE cl.language = 'English' AND cl.isOfficial = 'true';
 
 -- 9. The names of all of the continents and the population of the continent’s largest city
 -- (6 rows, largest population for North America: 8,591,309)
+SELECT co.continent, MAX(c.population) AS largest_city_population
+FROM city c 
+JOIN country co ON co.code = c.countrycode
+GROUP BY co.continent;
 
 -- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
 -- (29 rows)
+SELECT c.name, cl.language
+FROM city c
+JOIN country co ON c.countrycode = co.code 
+JOIN countrylanguage cl ON cl.countrycode = co.code
+WHERE co.continent = 'South America' AND c.population > 1000000 AND cl.isOfficial = 'true';
+
